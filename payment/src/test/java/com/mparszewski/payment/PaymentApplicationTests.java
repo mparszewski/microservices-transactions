ac4be9e6a765909d2aaa6ef80ffe06f47ea6ac79
Miłosz Marek Parszewski <mparszewski@gmail.com>
2020-12-10 13:24:51 +0100

    Payment service added

---
+++ payment/src/test/java/com/mparszewski/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mparszewski.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
