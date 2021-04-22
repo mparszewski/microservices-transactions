5f46f9745bfc2a0274e8fca53e9cd86516b7f45e
Miłosz Marek Parszewski <mparszewski@gmail.com>
2021-04-22 17:11:45 +0200

    Added service-discovery

---
+++ service-discovery/src/test/java/com/mparszewski/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mparszewski.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
